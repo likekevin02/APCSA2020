ad07f94b432b05cbc20db9be6e48cb35580888cd
leek6287 <leek6287@CA-SU-F106-12.sduhsd.lan>
2020-03-02 11:12:31 -0800

    Elevens lab card and deck completed & Unit 11 uploaded (School)

--- APCS-A_2020/src/elevens/Card.java
+++ APCS-A_2020/src/elevens/Card.java
@@ -11,15 +11,24 @@ public class Card
 	//instance variables
 		private String suit;
 		private int face;
+		private String rank;
+		private int pointValue;
+
 
   	//constructors
 		public Card(){
 			suit = "SPADES";
 			face = 1;
+			rank = "ACE";
+			pointValue = 1;
 		}
 		
 		public Card(String startSuit, int startFace) {
 			setCard(startSuit, startFace);
+		}
+		
+		public Card(String startSuit, String startRank, int startPointValue) {
+			setCard(startSuit, startRank, startPointValue);
 		}
 
 	// modifiers
@@ -29,12 +38,26 @@ public class Card
 			face = startFace;
 		}
 		
+		public void setCard(String startRank, String startSuit, int startPointValue) {
+			rank = startRank;
+			suit = startSuit;
+			pointValue = startPointValue;
+		}
+		
 		public void setFace(int startFace) {
 			face = startFace;
 		}
 		
 		public void setSuit(String startSuit) {
 			suit = startSuit;
+		}
+		
+		public void setRank(String startRank) {
+			rank = startRank;
+		}
+		
+		public void setPointValue(int startPointValue) {
+			pointValue = startPointValue;
 		}
 
   	//accessors
@@ -46,12 +69,34 @@ public class Card
 		public int getFace() {
 			return face;
 		}
+		
+		public String getRank() {
+			return rank;
+		}
+		
+		public int getPointValue() {
+			return pointValue;
+		}
+		
 
 
   	//toString
 		
 		public String toString() {
+			if (pointValue > 0) {
+				return FACES[pointValue] + " of " + suit;
+			}
 			return FACES[face] + " of " + suit;
+		}
+		
+		public boolean matches(Card otherCard) {
+			if (this.getSuit().equals(otherCard.getSuit())&&this.getRank().equals(otherCard.getRank())&&this.pointValue==(otherCard.pointValue)) {
+				return true;
+			} else if(this.getSuit().equals(otherCard.getSuit())&&this.getRank().equals(otherCard.getRank())&&this.face==(otherCard.face)) {
+				return true;
+			}
+			
+			return false;
 		}
 
  }
--- APCS-A_2020/src/elevens/Deck.java
+++ APCS-A_2020/src/elevens/Deck.java
@@ -1,5 +1,6 @@
 package elevens;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
@@ -9,45 +10,100 @@ import java.util.Arrays;
 import java.util.Collections;
 
 //make a Deck class
-public class Deck{
+public class Deck {
 
 	public static final int NUMCARDS = 52;
 	public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
-	
+
 	private List<Card> cards;
 	private int top;
+	private int size;
 
-   //make a Deck constructor
+	// make a Deck constructor
 	public Deck() {
 		cards = new ArrayList<Card>();
 		top = 51;
-		
+
 		for (int i = 0; i < SUITS.length; i++) {
-			for(int j = 1; j <= 13; j++) {
-				Card newCard = new Card(SUITS[i],j);
+			for (int j = 1; j <= 13; j++) {
+				Card newCard = new Card(SUITS[i], j);
 				cards.add(newCard);
 			}
 		}
 	}
-   	//refer cards to new ArrayList
-   	//set top to the top of the deck 51
-   	
-   	//loop through all suits
-   		//loop through all faces 1 to 13
-   			//add a new TwentyOneCard to the deck
 
-   
-   //make a dealCard() method that returns the top cardS
-	
-	public Card dealCard() {
-		return cards.get(top--);
-	}
-   
-   //write a shuffle() method
-   	//use Colletions.shuffle
-   	//reset the top card 
-	
-	public void shuffle() {
+
+	public Deck(String[] ranks, String[] suits, int[] pointValues) {
+		cards = new ArrayList<Card>();
 		
+		for (int s = 0; s <suits.length; s++ ) {
+			for (int r = 0; r<ranks.length;r++) {
+				cards.add(new Card(ranks[r],suits[s],pointValues[r]));
+			}
+		}
+		
+		size = cards.size();
+		top = size-1;
+		shuffle();
 	}
+
+	public Card dealCard() {
+		Card topCard;
+		if (!isEmpty()) {
+			topCard = cards.get(top);
+		} else {
+			return new Card();
+		}
+
+		top--;
+		size--;
+		return topCard;
+	}
+
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public int size() {
+		return size;
+	}
+
+//write a shuffle() method
+	// use Colletions.shuffle
+	// reset the top card
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+	public void resetTop() {
+		size = cards.size();
+		top = size - 1;
+	}
+	
+	public String toString() {
+		String rtn = "size = " +size + "\nUndealt cards: \n";
+		
+		for (int k=size -1; k>=0;k--) {
+			rtn = rtn + cards.get(k);
+			if (k != 0) {
+				rtn = rtn +", ";
+			}
+			if ((size - k)% 2 ==0) {
+				rtn = rtn + "\n";
+			}
+		}
+		
+		rtn = rtn +"\nDealt cards: \n";
+		for (int k = cards.size()-1; k>= size; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != size) {
+				rtn = rtn+ ", ";
+			}
+		}
+		
+		return rtn + "\n";
+	}
+				
+
 }
